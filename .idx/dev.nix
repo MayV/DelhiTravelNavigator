70615d5fe08b5dd3436cb3f753811b1fcab70ddd
mrv2671998 <mrv2671998@gmail.com>
2024-08-23 08:54:59 +0000

    Modify file to test commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -23,7 +23,7 @@
           pip install -r mysite/requirements.txt
         '';
         # Open editors for the following files by default, if they exist:
-        default.openFiles = ["README.md" "mysite/mysite/urls.py"];
+        default.openFiles = ["README.md"];
       };
       # To run something each time the workspace is (re)started, use the `onStart` hook
     };
